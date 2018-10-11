7d9f1cc00b5c08d3e0b24e736fe97e90643fcc33
Adam Letizia <adamjletizia@gmail.com>
2018-10-10 19:38:55 -0500

    start ApiClient

---
+++ TechnicalShowcase.Services/Api/ApiClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using TechnicalShowcase.Services.Wrappers;
+
+namespace TechnicalShowcase.Services.Api
+{
+    public interface IApiClient
+    {
+        T Get<T>(string uri) where T : class;
+        IEnumerable<T> GetAll<T>(string uri) where T : class;
+    }
+    public class ApiClient : IApiClient
+    {
+        private readonly IHttpClientWrapper _httpClient;
+
+        public ApiClient(IHttpClientWrapper httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public T Get<T>(string uri) where T : class
+        {
+            _httpClient.GetAsync(uri);
+            return null;
+        }
+
+        public IEnumerable<T> GetAll<T>(string uri) where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TechnicalShowcase.Tests/Services/Api/ApiClientTest.cs
@@ -0,0 +1,39 @@
+﻿using Bogus;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TechnicalShowcase.Services.Api;
+using TechnicalShowcase.Services.Wrappers;
+
+namespace TechnicalShowcase.Tests.Services.Api
+{
+    [TestClass]
+    public class ApiClientTest
+    {
+        private ApiClient _apiClient;
+        private Mock<IHttpClientWrapper> _httpClientMock;
+        private Randomizer _random;
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            _random = new Randomizer();
+
+            _httpClientMock = new Mock<IHttpClientWrapper>();
+            _apiClient = new ApiClient(_httpClientMock.Object);
+        }
+
+        [TestClass]
+        public class GetTest : ApiClientTest
+        {
+            [TestMethod]
+            public void ShouldCallHttpClientWrapperWithUriPassedAsParameter()
+            {
+                var expectedUri = _random.Word();
+
+                _apiClient.Get<object>(expectedUri);
+
+                _httpClientMock.Verify(client => client.GetAsync(expectedUri), Times.Once);
+            }
+        }
+    }
+}
